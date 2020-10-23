d178a3e10980a43423718f8bc466f61a8bbb9edb
ts-miller <tylerstephen11@gmail.com>
2020-10-22 22:18:02 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,4 @@
 class Artist < ActiveRecord::Base
-  # add associations here
+  validates_presence_of :name
+  has_many :songs
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,4 @@
 class Genre < ActiveRecord::Base
-  # add associations
+  validates_presence_of :name
+  has_many :songs
 end
